5c6e404be51289dd2e78be97dbc66f1f8efece71
lili <1070386120@qq.com>
2020-08-25 21:52:06 +0800

    小球碰墙

--- src/MyTest.java
+++ src/MyTest.java
@@ -6,44 +6,110 @@ public class MyTest
     public static void main(String[] args)
     {
         JFrame w = new JFrame();
-        w.setSize(1024,768);
-        w.getContentPane().setBackground(Color.black);
+        w.setSize(600,600);
 
         MyPanel mp = new MyPanel();
-        w.getContentPane().add(mp);
-        mp.setOpaque(false);
+        w.add(mp);
 
         Thread t = new Thread(mp);
         t.start();
-
         w.setVisible(true);
+
     }
 }
 class MyPanel extends JPanel implements Runnable
 {
-    int x = 30;
-    int y = 30;
+    int att  =0;
+    int x = 100;
+    int y = 20;
+
+
+    Color c = new Color((int)(255*Math.random()),(int)(255*Math.random()),(int)(255*Math.random()));
     public void paint(Graphics g)
     {
-        g.setColor(Color.white);
+        super.paint(g);
+        g.setColor(c);
         g.fillOval(x,y,50,50);
     }
-
     public void run()
     {
-        while(true)
+        while (true)
         {
-            y++;
-            if(y>=700)
+            //定义飞行状态
+            if(att==0)
             {
-                y=0;
+                x++;
+                y++;
+
+            }else if(att==1)
+            {
+                x--;
+                y++;
+            }else if(att==2)
+            {
+                x--;
+                y--;
+            }else if(att==3)
+            {
+                x++;
+                y--;
+            }
+            //改变飞行状态
+            if (x>540)
+            {
+                c = new Color((int)(255*Math.random()),(int)(255*Math.random()),(int)(255*Math.random()));
+                if (att==0)
+                {
+                    att=1;
+                }
+                else
+                {
+                    att=2;
+                }
+            }
+            if (y>520)
+            {
+                c = new Color((int)(255*Math.random()),(int)(255*Math.random()),(int)(255*Math.random()));
+                if (att==1)
+                {
+                    att=2;
+                }
+                else
+                {
+                    att=3;
+                }
+            }
+            if (x<0)
+            {
+                c = new Color((int)(255*Math.random()),(int)(255*Math.random()),(int)(255*Math.random()));
+                if (att==2)
+                {
+                    att=3;
+                }
+                else
+                {
+                    att=0;
+                }
+            }
+            if (y<0)
+            {
+                c = new Color((int)(255*Math.random()),(int)(255*Math.random()),(int)(255*Math.random()));
+                if (att==3)
+                {
+                    att=0;
+                }
+                else
+                {
+                    att=1;
+                }
             }
             try
             {
                 Thread.sleep(10);
-            }catch (Exception e){};
+            }catch (Exception e){}
             repaint();
         }
+
 
     }
 }
