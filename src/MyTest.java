143fb83f7ad4db90d88ca6f6a91e8e8b257209eb
lili <1070386120@qq.com>
2020-08-26 19:25:54 +0800

    第二阶段项目开始------------- 三种布局

--- src/MyTest.java
+++ src/MyTest.java
@@ -1,66 +1,27 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 public class MyTest
 {
     public static void main(String[] args)
     {
         JFrame w = new JFrame();
-        w.setSize(1024,768);
+        w.setSize(100,200);
+        JButton bt1 = new JButton("1");
+        JButton bt2 = new JButton("2");
+        JButton bt3 = new JButton("388888888888888");
+        JButton bt4 = new JButton("4");
+        JButton bt5 = new JButton("5");
 
-        MyPanel mp = new MyPanel();
-        w.add(mp);
+        w.setLayout(new GridLayout(3,2));
+        w.add(bt1);
+        w.add(bt2);
+        w.add(bt3);
+        w.add(bt4);
+        w.add(bt5);
 
-        w.addMouseListener(mp);
-        mp.addMouseListener(mp);
 
         w.setVisible(true);
-    }
-}
-class MyPanel extends JPanel implements MouseListener
-{
-    Boolean clicked = true;
-    public void paint(Graphics g)
-    {
-        if (!clicked)
-        {
-
-            g.setColor(Color.black);
-            g.drawLine(30,30,100,30);
-            g.drawLine(30,30,30,80);
-            g.setColor(Color.white);
-            g.drawLine(100,30,100,80);
-            g.drawLine(30,80,100,80);
-
-        }
-        else
-        {
-            g.setColor(Color.white);
-            g.drawLine(30,30,100,30);
-            g.drawLine(30,30,30,80);
-            g.setColor(Color.black);
-            g.drawLine(100,30,100,80);
-            g.drawLine(30,80,100,80);
-        }
-
 
     }
-    public void mouseClicked(MouseEvent e)
-    {
-
-    }
-    public void mousePressed(MouseEvent e)
-    {
-        if (e.getX() >=30 && e.getX()<=100 && e.getY() >=30 && e.getX()<=80)
-        {
-            clicked = !clicked;
-            repaint();
-        }
-
-    }
-    public void mouseReleased(MouseEvent e){}
-    public void mouseEntered(MouseEvent e){}
-    public void mouseExited(MouseEvent e){}
 }
