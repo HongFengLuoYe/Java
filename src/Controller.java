72564805f4ac1d315232556a3477f4e4f92bca43
lili <1070386120@qq.com>
2020-08-29 02:08:22 +0800

    第二阶段项目开始------------- 读取字节流数据

--- src/Controller.java
+++ src/Controller.java
@@ -1,16 +1,30 @@
+import java.io.File;
+import java.io.FileInputStream;
+
 public class Controller
 {
 
     public static void main(String[] args)
     {
-        //登陆界面
-        QQLogin loginInterface = new QQLogin();
-        //登录成功后
-        loginInterface.loginSucceed = () ->
+//        //登陆界面
+//        QQLogin loginInterface = new QQLogin();
+//        //登录成功后
+//        loginInterface.loginSucceed = () ->
+//        {
+//           loginInterface.setVisible(false);
+//           QQmain  mainInterface = new QQmain();
+//       };
+        try
         {
-           loginInterface.setVisible(false);
-           QQmain  mainInterface = new QQmain();
-       };
+            File f = new File("D:\\work\\test.txt");
+            FileInputStream fis = new FileInputStream(f);
+            while(fis.available()>0)
+            {
+                System.out.println((char)fis.read());
+            }
+
+        }catch (Exception e){}
+
 
     }
 
